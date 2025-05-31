e5397d623aadb04589fa1e724dabbef959e0dca3
valhize <190005533+Kritzeey@users.noreply.github.com>
2025-05-31 11:09:38 +0700

    feat: init prisma

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,9 +5,10 @@ import { EventsModule } from "./events/events.module";
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
-  imports: [EventsModule, AuthModule, UsersModule, PrismaModule],
+  imports: [ConfigModule.forRoot(), EventsModule, AuthModule, UsersModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
