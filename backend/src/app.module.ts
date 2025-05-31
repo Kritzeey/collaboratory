7d864d89619bc5e728771e2cdd5f76f1f49144bb
valhize <190005533+Kritzeey@users.noreply.github.com>
2025-05-31 10:54:38 +0700

    feat: init modules

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { EventsModule } from "./events/events.module";
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [EventsModule],
+  imports: [EventsModule, AuthModule, UsersModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
